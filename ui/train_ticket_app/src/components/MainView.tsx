1d833f1397681b6e200e121b0107806a88241ca6
Berkay <berkaytrhn@gmail.com>
2025-01-16 21:59:56 +0300

    minor changes on selection ui

--- ui/train_ticket_app/src/components/MainView.tsx
+++ ui/train_ticket_app/src/components/MainView.tsx
@@ -1,5 +1,5 @@
-import { Grid2 as Grid} from "@mui/material";
-import { ReactNode } from "react";
+import { FormControl, Grid2 as Grid, MenuItem, Select, SelectChangeEvent} from "@mui/material";
+import { ReactNode, useState } from "react";
 import React from "react";
 
 interface MainViewProps{
@@ -7,10 +7,36 @@ interface MainViewProps{
 }
 
 const MainView=(props: MainViewProps)=>{
+
+
+    const [locationValue, setLocationValue] = useState("");
+
+    const locationList = [
+        "Ankara GAR",
+        "İstanbul (Söğütlüçeşme)",
+        "İstanbul (Pendik)"
+    ]
+
+    const handleChange = (event: SelectChangeEvent) => {
+        setLocationValue(event.target.value as string);
+      };
     return(
         <Grid container>
             <Grid size={12}>
                 <h4 style={{color: "red"}}>Scheduling Ticket Search View!!</h4>
+            </Grid>
+            <Grid>
+                <input type="date"></input>
+                <FormControl fullWidth>
+                    <Select
+                        fullWidth 
+                        value={locationValue}
+                        onChange={handleChange}>
+                        {locationList.map((element: string)=>{
+                            return <MenuItem key={`${element}_aa`} value={element}>{element}</MenuItem>
+                        })}
+                    </Select>
+                </FormControl>
             </Grid>
         </Grid>
     );
