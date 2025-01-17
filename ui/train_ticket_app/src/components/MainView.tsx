0affac7ea1eb89248ed9d730bdd2b000f996bec7
Berkay <berkaytrhn@gmail.com>
2025-01-17 23:29:46 +0300

    updated date input

--- ui/train_ticket_app/src/components/MainView.tsx
+++ ui/train_ticket_app/src/components/MainView.tsx
@@ -1,15 +1,25 @@
-import { FormControl, Grid2 as Grid, MenuItem, Select, SelectChangeEvent} from "@mui/material";
+import { FormControl, Grid2 as Grid, MenuItem, Select, SelectChangeEvent, TextField } from "@mui/material";
 import { ReactNode, useState } from "react";
 import React from "react";
+import { DatePicker } from '@mui/x-date-pickers/DatePicker';
+import { LocalizationProvider } from "@mui/x-date-pickers";
+import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
+import dayjs, { Dayjs } from "dayjs";
 
-interface MainViewProps{
-    children ?: ReactNode;
+
+import advancedFormat from "dayjs/plugin/advancedFormat";
+dayjs.extend(advancedFormat);
+
+interface MainViewProps {
+    children?: ReactNode;
 }
 
-const MainView=(props: MainViewProps)=>{
+const MainView = (props: MainViewProps) => {
 
 
     const [locationValue, setLocationValue] = useState("");
+
+    const [value, setValue] = useState<Dayjs | null>(dayjs()); // Use Dayjs type
 
     const locationList = [
         "Ankara GAR",
@@ -19,20 +29,27 @@ const MainView=(props: MainViewProps)=>{
 
     const handleChange = (event: SelectChangeEvent) => {
         setLocationValue(event.target.value as string);
-      };
-    return(
+    };
+    return (
         <Grid container>
             <Grid size={12}>
-                <h4 style={{color: "red"}}>Scheduling Ticket Search View!!</h4>
+                <h4 style={{ color: "red" }}>Scheduling Ticket Search View!!</h4>
             </Grid>
             <Grid>
-                <input type="date"></input>
+                <LocalizationProvider dateAdapter={AdapterDayjs}>
+                    <DatePicker
+                        label="Trip Day"
+                        value={value}
+                        onChange={(newValue: Dayjs | null) => setValue(newValue)}
+                        format="DD/MM/YYYY" // Set the format to dd/MM/yyyy
+                        />
+                </LocalizationProvider>
                 <FormControl fullWidth>
                     <Select
-                        fullWidth 
+                        fullWidth
                         value={locationValue}
                         onChange={handleChange}>
-                        {locationList.map((element: string)=>{
+                        {locationList.map((element: string) => {
                             return <MenuItem key={`${element}_aa`} value={element}>{element}</MenuItem>
                         })}
                     </Select>
